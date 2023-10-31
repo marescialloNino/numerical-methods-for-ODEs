9d5ff1a37650f3a3bc2836f03db549f5adcecbfa
KG\ukg11058 <nicola.schiavo@kireygroup.com>
2023-10-31 10:55:00 +0100

    feat(runge_kutta_4): excersise 2 function finished

--- runge_kutta_4.py
+++ runge_kutta_4.py
@@ -18,11 +18,9 @@ def rk4(f, t, y, T, h, sol = None):
 	
 		# n: number of steps
 		n = int((T-t)/h)
-		# define dataframe to store data
+		# define array to store data
 		results = []
-		#columns = ["t", "Estimate", "Exact", "Error"]
-		#results_df = pd.DataFrame(columns=columns)
-		#first_row = pd.DataFrame({ 't': [t], 'Estimate': [y], "Exact":[actual], "Error": [error] })
+
 		# loop to calculate the values of the parameters k at each step
 		for i in range(n):
 			k1 = h*f(t, y)
@@ -31,16 +29,14 @@ def rk4(f, t, y, T, h, sol = None):
 			k4 = h*f(t + h, y + k3)
 			y = y + (k1 + 2*k2 + 2*k3 + k4)/6
 			t += h
+			# if analytical solution is given --> compare estimate ad exact solution
 			if sol is not None:
 				actual = sol(t)
 				error = abs(actual - y)
 				results.append([t, y, actual, error])
-				#df_new_row = pd.DataFrame({ 't': [t], 'Estimate': [y], "Exact":[actual], "Error": [error] })
+			# if not, just estimate the solution
 			else:
-				results.append([t, y])
-				#df_new_row = pd.DataFrame({ 't': [t], 'Estimate': [y]})
-			#results_df = pd.concat([results_df, df_new_row],ignore_index=True)
-			
+				results.append([t, y])		
 
 		if sol is not None:
 			columns = ["t", "Estimate", "Exact", "Error"]
@@ -49,34 +45,37 @@ def rk4(f, t, y, T, h, sol = None):
 
 		return pd.DataFrame(results, columns=columns)	 
 
-# main function depending on the ODE to solve
-# returns the an error dataframe and a log log plot of the final error
-# in function of the number of steps n
-def main():
+""" 
+	excersise2 function depending on the ODE to solve
+	returns an error dataframe and a log log plot of the final error
+ 	in function of the number of steps n
+"""
+def excersise2():
 
-	# Define these functions depending on the problem.
+	# Define these functions depending on the problem to solve.
 	f = lambda t, y: -5*y
 	exact_sol= lambda t: exp(-5*t)
 	t0 = 0
 	y0 = 1
 	T = 1
-	# Define 
-	error_columns = ["h", "n", "Final Error"]
+	
+	# define table of stepsize, number of steps and error.
+	error_columns = ["h", "n", "final_error"]
 	error_df = pd.DataFrame(columns=error_columns)
 
+	# define an array of steps, h = 2^(-k) for k=5,6,7,8,9,10.
 	steps = [2**(-k) for k in range(5,11)]
 
+	# calculate rk4 results at each given step, storing the error at final time in the error table.
 	for h in steps:
-		
 		results = rk4(f, t0, y0, T, h, exact_sol) 
 		final_error = results["Error"].iat[-1]	
-		error_df_new_row = pd.DataFrame({'h':[h], "n":[int((T-t0)/h)], "Final Error":[final_error]})
+		error_df_new_row = pd.DataFrame({'h':[h], "n":[int((T-t0)/h)], "final_error":[final_error]})
 		error_df = pd.concat([error_df, error_df_new_row], ignore_index=True)
-		#print(results)
 
 	print(error_df)
 	plt.loglog(error_df.n, error_df["Final Error"])
 	plt.show()
 
 if __name__ == '__main__':
-	main()
+	excersise2()
