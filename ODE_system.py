90df80c6577691d09cd5f31362a4544cf7d7aac7
RichardNixon <nicola.schiavo@studenti.unipd.it>
2023-11-14 20:49:48 +0100

    update

--- ODE_system.py
+++ ODE_system.py
@@ -7,16 +7,14 @@ import matplotlib.pyplot as plt
 
 from scipy.sparse import eye
 from scipy.integrate import solve_ivp
-
+from scipy.sparse import diags, kron
 
 
 accurate_final_solution = np.genfromtxt(open("exact_sol.csv"), delimiter=",", dtype=float)
 
-from scipy.sparse import diags, kron
-from scipy.sparse.linalg import spsolve
-
+#---------------------------------DISCRETE LAPLACIAN--------------------------------------------
 def discrete_laplacian(n):
-    """Create a discrete Laplacian for a square grid of size n x n."""
+    """Create a discrete Laplacian Matrix for a square grid of size n x n."""
     # Create a 1D discrete Laplacian
     laplacian_1d = diags([1, -2, 1], [-1, 0, 1], shape=(n, n))
 
@@ -29,30 +27,11 @@ def discrete_laplacian(n):
 nx = 100  # grid size
 A = -discrete_laplacian(nx-2) * (nx - 1)**2  # scale the Laplacian
 
-
-
-
-
-
-
-
-
-""" nx = 100
-# Compute the discrete Laplacian
-A = -(sp.diags([1, 1, -4, 1, 1], [-(nx-1), -1, 0, 1, (nx-1)], shape=((nx-2)**2, (nx-2)**2))*(nx - 1)**2)
-# Scale the matrix 
- """
-
-#------------------------------------------------------TEST LAPLACIAN MATRIX-------------------------------------------
-""" A = sp.diags([-1, -1, 4, -1, -1], [-4, -1, 0, 1, 4], shape=(4*4,4*4))
-A_dense = A.toarray()
-print(A_dense) """
-#----------------------------------------------------------------------------------------------------------------------
 # Find the largest magnitude eigenvalue
 # Using 'LM' (Largest Magnitude) mode 
 # spla.eigs() returns an array of eigenvalues (ordered by largest modulus)and one of eigenvectors
 lambda_, _ = spla.eigs(A, k=1, which='LM')
-lambda_ = lambda_[0].real
+lambda_ = -lambda_[0].real
 
 print("Largest magnitude eigenvalue:", lambda_)
 
@@ -94,8 +73,7 @@ plt.show() """
 n = A.shape[0]  
 y0 = np.ones(n)
 
-
-# Define the ODE function
+# Define the system ODE function
 def funct(t, y, A):
     return -A @ y
 
@@ -114,10 +92,8 @@ num_steps_ode45 = len(sol.t)
 # Error calculation
 err_ode45 = np.max(np.abs(sol.y[:, -1] - accurate_final_solution))
 
-print(err_ode45)
-
 # Display results
-run1 = ["run 1", "rk45", num_steps_ode45,err_ode45, CPU_time_ode45]
+run1 = ["run 1", "rk45", num_steps_ode45, err_ode45, CPU_time_ode45]
 print(run1)
 
 # ----------------------------------------------------Crank-Nicolson method------------------------------------------
@@ -138,7 +114,7 @@ for step in h:
     tStart = time.time()
     for i in range(N-1):
         b = (eye(n) - 0.5 * step * A) @ y_cn[:, i]
-        y_cn[:, i + 1], _ = spla.cg(this_A, b, tol=step**3, maxiter=500)
+        y_cn[:, i + 1], _ = spla.cg(this_A, b, tol=step**3, maxiter=1000)
     tEnd = time.time()
 
     CPU_time_CN.append(tEnd - tStart)
@@ -158,11 +134,13 @@ for step in h:
     this_A = eye(n) + (6/11) * step * A
     y_bdf = np.zeros((n, N+1))
 
+    # set initial 3 values to solution matrix of BDF3
     j=0
     y_bdf[:, 0] = y0
     y_bdf[:, 1] = y_start2[j]
     y_bdf[:, 2] = y_start3[j]
     j+=1
+    
     tStart = time.time()
     for i in range(1, N - 3):
         b = (18/11) * y_bdf[:, i + 2] - (9/11) * y_bdf[:, i + 1] + (2/11) * y_bdf[:, i]
@@ -177,11 +155,11 @@ for step in h:
 runs = [
     ["run 1", "ode45", num_steps_ode45, CPU_time_ode45],
     ["run 2", "crank-nicolson", num_steps_CN[0],err_CN[0], CPU_time_CN[0]],
-    ["run 1", "crank-nicolson", num_steps_CN[1], err_CN[1],CPU_time_CN[1]],
-    ["run 1", "crank-nicolson", num_steps_CN[2],err_CN[2], CPU_time_CN[2]],
-    ["run 1", "BDF3", num_steps_BDF3[0], err_BDF3[0],CPU_time_BDF3[0]],
-    ["run 1", "BDF3", num_steps_BDF3[1],err_BDF3[1], CPU_time_BDF3[1]],
-    ["run 1", "BDF3", num_steps_BDF3[2],err_BDF3[2], CPU_time_BDF3[2]]
+    ["run 3", "crank-nicolson", num_steps_CN[1], err_CN[1],CPU_time_CN[1]],
+    ["run 4", "crank-nicolson", num_steps_CN[2],err_CN[2], CPU_time_CN[2]],
+    ["run 5", "BDF3", num_steps_BDF3[0], err_BDF3[0],CPU_time_BDF3[0]],
+    ["run 6", "BDF3", num_steps_BDF3[1],err_BDF3[1], CPU_time_BDF3[1]],
+    ["run 7", "BDF3", num_steps_BDF3[2],err_BDF3[2], CPU_time_BDF3[2]]
     
 ]
 
