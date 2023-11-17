e010523028152441f708c631a2b92be1e313ce88
RichardNixon <nicola.schiavo@studenti.unipd.it>
2023-11-17 12:38:55 +0100

    final update

--- ODE_system.py
+++ ODE_system.py
@@ -10,7 +10,7 @@ from scipy.integrate import solve_ivp
 from scipy.sparse import diags, kron
 
 
-accurate_final_solution = np.genfromtxt(open("exact_sol.csv"), delimiter=",", dtype=float)
+
 
 #---------------------------------DISCRETE LAPLACIAN--------------------------------------------
 def discrete_laplacian(n):
@@ -24,22 +24,8 @@ def discrete_laplacian(n):
 
     return laplacian_2d
 
-nx = 100  # grid size
-A = -discrete_laplacian(nx-2) * (nx - 1)**2  # scale the Laplacian
-
-# Find the largest magnitude eigenvalue
-# Using 'LM' (Largest Magnitude) mode 
-# spla.eigs() returns an array of eigenvalues (ordered by largest modulus)and one of eigenvectors
-lambda_, _ = spla.eigs(A, k=1, which='LM')
-lambda_ = -lambda_[0].real
-
-print("Largest magnitude eigenvalue:", lambda_)
-
-
 #---------------------------------------------STABILITY-------------------------------------------
-# for stability h must be less then this limit value
-h_max = -2.78 / lambda_
-print(f'step size must be less then {h_max:9.4e} for stability ')
+
 
 """ # stability characteristic equation
 def R(z):
@@ -69,42 +55,28 @@ plt.grid()
 plt.show() """
 
 #-------------------------------------------------------------------------------------------------------------
-# number of rows in the matrix A
-n = A.shape[0]  
-y0 = np.ones(n)
+
 
 # Define the system ODE function
 def funct(t, y, A):
     return -A @ y
 
-#  Solve the ODE
-#  The function scipy.integrate.solve_ivp uses the method RK45 by default,
-#  similar the method used by Matlab's function ODE45 as both use the
-#  Dormand-Pierce formulas with fourth-order method accuracy.
-tspan = [0, 0.1]
-tStart = time.time()
-sol = solve_ivp(funct, tspan, y0, args=(A,), method='RK45')
-tEnd = time.time()
-
-CPU_time_ode45 = tEnd - tStart
-num_steps_ode45 = len(sol.t)
-
-# Error calculation
-err_ode45 = np.max(np.abs(sol.y[:, -1] - accurate_final_solution))
-
-# Display results
-run1 = ["run 1", "rk45", num_steps_ode45, err_ode45, CPU_time_ode45]
-print(run1)
-
 # ----------------------------------------------------Crank-Nicolson method------------------------------------------
-h = [0.001, 0.0001, 0.00001]
-CPU_time_CN = []
-num_steps_CN = []
-err_CN = []
-y_start2 = []
-y_start3 = []
 
-for step in h:
+def CN(t0, T, h, y0, f, A):
+    N = int((T-t0)/h)
+    N = N+1
+    t = np.linspace(t0, T, N)
+    m = np.shape(y0)[0]
+    y = np.zeros([N, m])
+    y[0] = y0
+    thisA = eye(np.shape(A)[0])+(h/2)*A
+    for i in range(N-1):
+        b = (eye(np.shape(A)[0])-(h/2)*A).dot(y[i])
+        y[i+1] = spla.cg(A=thisA, b=b.T, x0=y[i], tol=h**3)[0]
+    return t, y
+
+""" for step in h:
     this_A = eye(n) + 0.5 * step * A
     # number of steps
     N = round(0.1 / step)
@@ -113,7 +85,7 @@ for step in h:
 
     tStart = time.time()
     for i in range(N-1):
-        b = (eye(n) - 0.5 * step * A) @ y_cn[:, i]
+        b = (eye(n) - 0.5 * step * A).dot(y_cn[:, i])
         y_cn[:, i + 1], _ = spla.cg(this_A, b, tol=step**3, maxiter=1000)
     tEnd = time.time()
 
@@ -122,13 +94,29 @@ for step in h:
     y_start2.append(y_cn[:, 1])
     y_start3.append(y_cn[:, 2])
     err_CN.append(np.max(np.abs(y_cn[:, -1] - accurate_final_solution)))
+ """
 
 # -------------------------------------------------------BDF3 method-----------------------------------------------------
-CPU_time_BDF3 = []
-num_steps_BDF3 = []
-err_BDF3 = []
 
-for step in h:
+
+# I define BDF3 method for this particular function
+def BDF3(t0, tN, h, y0, f, A):
+    N = int((tN-t0)/h)
+    N = N+1
+    t = np.linspace(t0, tN, N)
+    m = np.shape(y0)[0]
+    y = np.zeros([N, m])
+    y[0] = y0
+    thisA = eye(np.shape(A)[0])+(h*6/11)*A
+
+    solve = solve_ivp(f, [t0, t0+2*h], y0, t_eval=np.linspace(t0, t0+2*h, 3), args=(A,))
+    y[:3] = solve.y.T
+    for i in range(N-3):
+        b = (18/11)*y[i+2]-(9/11)*y[i+1]+(2/11)*y[i]
+        y[i+3] = spla.cg(A=thisA, b=b.T, x0=y[i], tol=h**3)[0]
+    return t, y
+
+""" for step in h:
 
     N = round(0.1 / step)
     this_A = eye(n) + (6/11) * step * A
@@ -150,22 +138,78 @@ for step in h:
     CPU_time_BDF3.append(tEnd - tStart)
     num_steps_BDF3.append(N)
     err_BDF3.append(np.max(np.abs(y_bdf[:, -1] - accurate_final_solution)))
+ """
 
-# Prepare the final table
-runs = [
-    ["run 1", "ode45", num_steps_ode45, CPU_time_ode45],
-    ["run 2", "crank-nicolson", num_steps_CN[0],err_CN[0], CPU_time_CN[0]],
-    ["run 3", "crank-nicolson", num_steps_CN[1], err_CN[1],CPU_time_CN[1]],
-    ["run 4", "crank-nicolson", num_steps_CN[2],err_CN[2], CPU_time_CN[2]],
-    ["run 5", "BDF3", num_steps_BDF3[0], err_BDF3[0],CPU_time_BDF3[0]],
-    ["run 6", "BDF3", num_steps_BDF3[1],err_BDF3[1], CPU_time_BDF3[1]],
-    ["run 7", "BDF3", num_steps_BDF3[2],err_BDF3[2], CPU_time_BDF3[2]]
+
+def main():
+
+    nx = 100  # grid size
+    A = -discrete_laplacian(nx-2) * (nx - 1)**2  # scale the Laplacian
+
+    # Find the largest magnitude eigenvalue
+    # Using 'LM' (Largest Magnitude) mode 
+    # spla.eigs() returns an array of eigenvalues (ordered by largest modulus)and one of eigenvectors
+    lambda_, _ = spla.eigs(A, k=1, which='LM')
+    lambda_ = -lambda_[0].real
+
+    print("Largest magnitude eigenvalue:", lambda_)
+
+    # for stability h must be less then this limit value
+    h_max = -2.78 / lambda_
+    print(f'step size must be less then {h_max:9.4e} for stability ')
+
+    # number of rows in the matrix A
+    n = A.shape[0]  
+    y0 = np.ones(n)
     
-]
+    t0 = 0
+    T = 0.1
+    h = [0.001, 0.0001, 0.00001]
+    runs = []
+    accurate_final_solution = np.genfromtxt(open("exact_sol.csv"), delimiter=",", dtype=float)
 
-head = ["RUN", "METHOD", "NUMBER OF STEPS", "ERROR", "CPU TIME"]
-TABLE = [head] + runs
+    #  The function scipy.integrate.solve_ivp uses the method RK45 by default,
+    #  similar the method used by Matlab's function ODE45 as both use the
+    #  Dormand-Pierce formulas with fourth-order method accuracy.
+    
+    tStart = time.time()
+    sol = solve_ivp(funct,[t0,T], y0, args=(A,), method='RK45')
+    tEnd = time.time()
 
-# Display the table
-for row in TABLE:
-    print(row)
+    CPU_time_ode45 = tEnd - tStart
+    num_steps_ode45 = len(sol.t)
+    err_ode45 = np.max(np.abs(sol.y[:, -1] - accurate_final_solution))
+
+    runs.append(["rk45", num_steps_ode45, err_ode45, CPU_time_ode45]) 
+
+    
+    for step in h:
+        num_steps = int((T-t0)/step)
+        start = time.time()
+        yCN_final = CN(t0, T, step, y0, funct, A=A)[1][-1, :]
+        end = time.time()
+        CPU_time = end-start
+        errCN = np.linalg.norm(accurate_final_solution - yCN_final, ord=np.inf)
+        runs.append(["Crank-Nicolson",num_steps,errCN,CPU_time])
+
+        
+    for step in h:
+        num_steps = int((T-t0)/step)
+        tic = time.time()
+        yNBDF_final = BDF3(t0, T, step, y0, funct, A=A)[1][-1, :]
+        toc = time.time()
+        CPU_time = toc-tic
+        errBDF3 = np.linalg.norm(accurate_final_solution - yNBDF_final, ord=np.inf)
+        runs.append(["BDF3",num_steps,errBDF3,CPU_time])
+
+    head = ["METHOD", "NUMBER OF STEPS", "ERROR", "CPU TIME"]
+    TABLE = [head] + runs
+
+    # Display the table
+    for row in TABLE:
+        print(row) 
+        
+
+
+if __name__ == '__main__':
+	main()
